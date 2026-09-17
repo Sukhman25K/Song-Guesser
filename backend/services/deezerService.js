624e84408986e25021072e2ca5e85e86e279e311
Sukhman Kang <sukhman.25k@gmail.com>
2026-09-17 17:11:55 +0100

    Add caching feature for genres to improve load time

--- backend/services/deezerService.js
+++ backend/services/deezerService.js
@@ -1,14 +1,27 @@
 import axios from "axios";
 
 const DEEZER_BASE = "https://twilight-snow-6f4a.sukhman-25k-279.workers.dev";
+const genreCache = {data: null, timestamp: 0};
+const GENRE_CACHE_DURATION = 24 * 60 * 60 * 1000;
 
 export async function fetchGenres() {
+    const now = Date.now();
+
+    if (genreCache.data && now - genreCache.timestamp < GENRE_CACHE_DURATION) {
+        return genreCache.data;
+    }
+
     const response = await axios.get(`${DEEZER_BASE}/genre`);
-    return response.data.data.map(genre => ({
+    const genres =  response.data.data.map(genre => ({
             id: genre.id,
             name: genre.name,
             picture: genre.picture_medium
     }));
+
+    genreCache.data = genres;
+    genreCache.timestamp = now;
+
+    return genres;
 }
 
 export async function fetchChartTracks(genreId, index, limit) {
